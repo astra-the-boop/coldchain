7d483a942b7aa97fc6bc961f4bcd9db35a225e1b
astra.celestine <109295196+astra-the-boop@users.noreply.github.com>
2026-04-16 22:30:05 +0700

    kbjdejbfrjksjvfsd

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import fs = require("fs");
 import path = require("path");
+import cheerio = require("cheerio");
+import axios from "axios";
 
 function getFiles(dir): string[]{
     let results:string[] = [];
@@ -19,6 +21,32 @@ function getFiles(dir): string[]{
     return results
 }
 
+function findImg(html){
+    const $ = cheerio.load(html);
+    const imgs = [];
+
+    $("img").each((_, el) => {
+        const src = $(el).attr("src");
+        if(src && src.startsWith("http")){
+            imgs.push(src)
+        }
+    });
+
+    return imgs;
+}
 
 
+async function download(url, output) {
+    const response = await axios({
+        url, method: "GET", responseType: "stream",
+    });
+
+    return new Promise((res, rej) => {
+        const writer = fs.createWriteStream(output);
+        response.data.pipe(writer);
+
+        writer.on("finish", res);
+        writer.on("error", rej);
+    })
+}
 // console.log(getFiles("/Users/astra.celestine/Desktop/site"));
